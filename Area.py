f0c60213684e1bce5e14ee0f6b58fdb56aec8d8e
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-16 18:37:47 +0530

    area of traingle ,square and circle

---
+++ Area.py
@@ -0,0 +1,15 @@
+#r = 5
+area=2*3.14*4
+print("area of circle",area)
+
+# area of traingle
+side = 3
+area= side*side*side
+print("area of triangle",area)
+
+#area of square
+
+side = 5
+area = side*side
+print("area of square",area)
+
