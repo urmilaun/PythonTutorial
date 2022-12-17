e1706afd265296efbc9d87b1d77e489e0bcf47fa
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-17 10:15:22 +0530

    Swapping of two number

---
+++ Swap.py
@@ -0,0 +1,8 @@
+# swapping of two number
+a=int(input("Enter the number a:"))
+b=int(input("Enter the number b:"))
+temp = a
+a = b
+b = temp
+print("Enter the value",":",a)
+print("Enter the value ",":",b)
