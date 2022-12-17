fd821adb97ccf47dbf92c850fbc4396f93841b53
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-17 10:42:03 +0530

    conversion Kilometer to miles

---
+++ KmtoMiles.py
@@ -0,0 +1,7 @@
+#1 kilometre equals 0.62137 miles.  
+#Miles = kilometre * 0.62137  
+#Kilometre = Miles / 0.62137  
+
+km = float(input("Enter the speed of bike in km"))
+miles = km * 0.6211371
+print("Enter the speed of bike in miles",miles)
