10ff25eddff951a8e89b0893f2e582a4b09c864a
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-17 10:29:36 +0530

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,13 @@
+#generate random number 
+#first import the random module
+#random module provide a float number
+import random
+n=random.random()
+print(n)
+
+
+#Generating number within a given range
+# the randint() method are generate integer number given a range
+import random
+n=random.randint(0,10)
+print(n)
