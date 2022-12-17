6c696d249f95df06e721cf80c13a48c1261f911d
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-17 11:00:39 +0530

    Arithmetic operation in python

---
+++ Arithmetic.py
@@ -0,0 +1,29 @@
+# addition of two numbers 
+a = int(input("enter a number a:"))
+b = int(input("Enter a number b:"))
+c=a+b
+print(c)
+
+# substraction of two number
+a = int(input("enter a number a:"))
+b = int(input("Enter a number b:"))
+c=a-b
+print(c)
+
+#multiplication of two number
+a = int(input("enter a number a:"))
+b = int(input("Enter a number b:"))
+c=a*b
+print(c)
+
+# division of two number
+a = int(input("enter a number a:"))
+b = int(input("Enter a number b:"))
+c=a/b
+print(c)
+
+#modulas of two number
+a = int(input("enter a number a:"))
+b = int(input("Enter a number b:"))
+c=a%b
+print(c)
