34f6fbbc23343be6f6eab3df480cdbbf7b926bae
Urmila <103355024+urmilaun@users.noreply.github.com>
2023-01-03 17:59:13 +0530

    Add files via upload

---
+++ filehandle.py
@@ -0,0 +1,20 @@
+f=open("myfiledemo.txt","r")
+data=f.read()
+i=1
+while(i!=0): 
+    rollno = int(input("roll no:"))
+    qualification = input("qualification:")  
+    name=input("name")
+    percentage=int(input("percentage:")) 
+    i=int(input("wants to add more yes/no"))
+
+
+
+
+
+
+
+
+
+
+
