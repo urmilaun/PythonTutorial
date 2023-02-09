3f389550150fc8ce53a7e2f34c8427280fb76b22
Urmila <103355024+urmilaun@users.noreply.github.com>
2023-02-09 12:35:37 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -13,3 +13,15 @@ class Customers():
         mycursor.execute("insert into customers(customer_name,account_number,mobile_number,email,account_type,account_balance,password) values('"+n+"',"+str(acc_num)+",'"+str(mob_num)+"','"+str(email)+"','"+str(acc_type)+"',"+str(acc_bal)+",'"+str(password)+"')")
         con.commit()
         print("Successfully added")
+
+   def Logic(self):
+        a=int(input("enter account number"))
+        p=input("enter password")
+        con=mysql.connector.connect(host="localhost",user="root",password="",database="bank")
+        mycursor=con.cursor()
+        mycursor.execute("select * from customer where account_number="+str(a)+"and password="+p+"")
+        data=mycursor.fetchall()
+        if(len(data)>0):
+            print(data)
+        else:
+            print("Check the account number and password")
