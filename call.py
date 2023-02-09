9f835d25aeee70a918b470ffad74c035479a9a63
Urmila <103355024+urmilaun@users.noreply.github.com>
2023-02-09 12:38:00 +0530

    Update call.py

--- call.py
+++ call.py
@@ -2,9 +2,11 @@ from  bank import Customers
 c=Customers()
 i=1
 while(i!=0):
-    ch=int(input("Enter the Choice:\n1. adduser"))
+    ch=int(input("Enter the Choice:\n1. adduser\n2.Logic"))
     if(ch==1):
         c.adduser()
+    elif(ch==2):
+        c.Logic()
         
     
 
